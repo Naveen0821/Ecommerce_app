1dbdd34bb65ffce604da18d0c399512c61b4bd5e
naveen <0821naveenk@gmail.com>
2023-12-14 12:00:07 +0530

    first commit

---
+++ category/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=255)
+    
